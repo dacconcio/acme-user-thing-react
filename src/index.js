347a55614ffd1deea3864188606ee8178510709f
David Acconcio <dacconcio@gmail.com>
2018-08-20 19:39:54 -0400

    circle

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const ListUsers = function(props) {
 		<div>
 			<div onClick={props.changeDisplayFunc}> {props.message} </div>
 
-			<div className="container">
+			<div className="container circle" >
 				{props.users.map(user => {
 					return (
 						<div key={user.id} className="circle">
